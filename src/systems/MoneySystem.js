2fe5b3b4f035877bce845fbb70f967573cc3a58b
Jongkyun Kim <qnftk0702@gmail.com>
2026-06-01 20:40:45 +0900

    돈 hud단순화

--- src/systems/MoneySystem.js
+++ src/systems/MoneySystem.js
@@ -3,10 +3,6 @@ export default class MoneySystem {
     this.scene = scene;
     this.money = 0;
     this.uiElements = {
-      count10000: document.querySelector("#count10000"),
-      count1000: document.querySelector("#count1000"),
-      count500: document.querySelector("#count500"),
-      count100: document.querySelector("#count100"),
       totalMoney: document.querySelector("#totalMoney"),
     };
   }
@@ -27,15 +23,9 @@ export default class MoneySystem {
   }
 
   updateUI() {
-    const count10000 = Math.floor(this.money / 10000);
-    const count1000 = Math.floor((this.money % 10000) / 1000);
-    const count500 = Math.floor((this.money % 1000) / 500);
-    const count100 = Math.floor((this.money % 500) / 100);
-    this.uiElements.count10000.textContent = count10000;
-    this.uiElements.count1000.textContent = count1000;
-    this.uiElements.count500.textContent = count500;
-    this.uiElements.count100.textContent = count100;
-    this.uiElements.totalMoney.textContent = `\u20A9${this.money.toLocaleString()}`;
+    if (this.uiElements.totalMoney) {
+      this.uiElements.totalMoney.textContent = `${this.money.toLocaleString()}원`;
+    }
     this.scene.uiManager?.updateNextQuestHint?.();
   }
 
