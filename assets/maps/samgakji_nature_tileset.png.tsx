00251cda3cb3c6dd324b371fb358b46e8069984c
Jongkyun Kim <qnftk0702@gmail.com>
2026-06-03 11:03:47 +0900

    꽃 화단 수정

--- assets/maps/samgakji_nature_tileset.png.tsx
+++ assets/maps/samgakji_nature_tileset.png.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.12.1" name="samgakji_nature_tileset.png" tilewidth="32" tileheight="32" tilecount="832" columns="32">
- <image source="../../../../Downloads/타일.png" width="1052" height="842"/>
+<tileset version="1.10" tiledversion="1.12.1" name="samgakji-tiles4" tilewidth="32" tileheight="32" tilecount="64" columns="8">
+ <image source="../../../../Downloads/samgakji-tiles4.png" width="256" height="256"/>
 </tileset>
